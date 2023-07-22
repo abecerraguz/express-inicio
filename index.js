2a4693b4f28ffbc6b3a35141f11ef15fd50e5239
Alejandro Mauricio Becerra Guzman <abecerraguz@MacBook-Pro-de-Alejandro.local>
2023-07-21 21:22:39 -0400

    Se guardan los cambios

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ app.get('/hola',(req,res)=>{
 
 
 
-app.listen( 3000,()=> console.log(`Servidor levantado en el puerto 3000`) )
+app.listen( process.env.PORT ,() => console.log(`Servidor levantado en el puerto 3000`) )
 
 // import http from 'http';
 // const servidor =  http.createServer((req,res) =>{
